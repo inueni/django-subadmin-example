0718d830fe83611c73887fcc07fb42f79cf9f1e9
Mitja Pagon <mitja.pagon@inueni.com>
2020-02-27 09:39:21 +0100

    Updated for django-subadmin 2.0.0

--- subadmin_example/management/commands/fakedata.py
+++ subadmin_example/management/commands/fakedata.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.core.management.base import BaseCommand
 
 from subadmin_example.models import MailingList, Subscriber, Message, Attachment
--- subadmin_example/models.py
+++ subadmin_example/models.py
@@ -1,36 +1,33 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.db import models
 
 
 class MailingList(models.Model):
     name = models.CharField(max_length=100)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
 
 
 class Subscriber(models.Model):
-    mailing_list = models.ForeignKey(MailingList)
+    mailing_list = models.ForeignKey(MailingList, on_delete=models.CASCADE)
     username = models.CharField(max_length=100)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.username
 
 
 class Message(models.Model):
-    mailing_list = models.ForeignKey(MailingList)
-    subscriber = models.ForeignKey(Subscriber)
+    mailing_list = models.ForeignKey(MailingList, on_delete=models.CASCADE)
+    subscriber = models.ForeignKey(Subscriber, on_delete=models.CASCADE)
     subject = models.CharField(max_length=100)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.subject
 
 
 class Attachment(models.Model):
-    message = models.ForeignKey(Message)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
     filename = models.CharField(max_length=100)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.filename
